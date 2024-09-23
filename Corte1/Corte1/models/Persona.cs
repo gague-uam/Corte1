a3d708b1652cba12c8dde86706c1846bbea7b19b
gague-UAM <ggpaiz@uamv.edu.ni>
2024-09-23 08:25:27 -0600

    Primera clase: Persona

---
+++ Corte1/Corte1/models/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.models
+{
+    internal class Persona
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string FechaNac { get; set; }
+        public string Ciudad { get; set; }
+    }
+}
