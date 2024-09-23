3ac9906ef4e21fb0b5e01856a6b64371f9f6fe28
gague-UAM <ggpaiz@uamv.edu.ni>
2024-09-23 08:46:58 -0600

    Tercera clase: Operacion

---
+++ Corte1/Corte1/models/Operacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.models
+{
+    internal class Operacion
+    {
+        public int CalcularEdad(DateTime fechaDeNacimiento)
+        {
+            int edad = DateTime.Now.Year - fechaDeNacimiento.Year;
+            if (DateTime.Now.DayOfYear < fechaDeNacimiento.DayOfYear)
+            {
+                edad--;
+            }
+            return edad;
+        }
+
+        public bool EsMayorDeEdad(DateTime fechaDeNacimiento)
+        {
+            int edad = CalcularEdad(fechaDeNacimiento);
+            return edad >= 18;
+        }
+    }
+}
